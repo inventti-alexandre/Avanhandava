3110934541ec76d60358a22975ef52dd9cf00905
jbalessandro <jb.alessandro@gmail.com>
2015-11-15 23:06:17 -0200

    Rotas

--- Avanhandava/App_Start/RouteConfig.cs
+++ Avanhandava/App_Start/RouteConfig.cs
@@ -13,10 +13,18 @@ namespace Avanhandava
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+            //    constraints: new[] { "Avanhandava.Controllers" }
+            //);
+
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "Avanhandava.Controllers" }
             );
         }
     }
